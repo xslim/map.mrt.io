7ef3425a674999f038500c8388cc9778d9f984f3
Taras Kalapun <t.kalapun@gmail.com>
2015-03-18 18:27:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-	var latlng = [51.505, -0.09];
-	var map = L.map('map').setView(latlng, 6);
-
-		
-
-		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'examples.map-i875mjb7'
-		}).addTo(map);
-
-		function onLocationFound(e) {
+var latlng = [51.505, -0.09];
+var map;
+  
+	function onLocationFound(e) {
 			var radius = e.accuracy / 2;
 
 			L.marker(e.latlng).addTo(map)
@@ -24,7 +14,29 @@
 			alert(e.message);
 		}
 
+  
+  function initmap() {
+    map = L.map('map').setView(latlng, 6);
+    
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'examples.map-i875mjb7'
+		}).addTo(map);
+		
 		map.on('locationfound', onLocationFound);
 		map.on('locationerror', onLocationError);
 
 		map.locate({setView: true, maxZoom: 16});
+  }
+
+
+		
+
+		
+
+	
+
+		

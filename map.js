12f409a830b429c3f7726b92c8fae96f926eb92a
Taras Kalapun <t.kalapun@gmail.com>
2017-08-29 12:33:01 +0200

    Update map.js

--- map.js
+++ map.js
@@ -19,18 +19,18 @@ function addLayers() {
   var subLayers = {}
   
   
-  baseMaps['OSM'] = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').;
+  baseMaps['OSM'] = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
   baseMaps['MapBox'] = L.tileLayer('http://{s}.tiles.mapbox.com/v3/xslim.hgm2p8g2/{z}/{x}/{y}.png');
   overlayMaps['OpenSeaMap'] = L.tileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png');
   
-  var transasToken = '9e53bcb2-01d0-46cb-8aff-512e681185a4'
-  var transasUrl = 'http://wms.transas.com/tms/1.0.0/{id}/{z}/{x}/{y}.png?token=' + transasToken
+  var transasToken = '9e53bcb2-01d0-46cb-8aff-512e681185a4';
+  var transasUrl = 'http://wms.transas.com/tms/1.0.0/{id}/{z}/{x}/{y}.png?token=' + transasToken;
   baseMaps['TX97'] = L.tileLayer(transasUrl, {id: 'tx97', maxZoom: 17, tms: true});
   baseMaps['UTT'] = L.tileLayer(transasUrl, {id: 'utt', maxZoom: 17, tms: true});
   overlayMaps['TX97-Transp'] = L.tileLayer(transasUrl, {id: 'tx97-transp', maxZoom: 17, tms: true});
   
   drawnItems = new L.FeatureGroup();
-  overlayMaps['Route'] = drawnItems
+  overlayMaps['Route'] = drawnItems;
   
   baseMaps['OSM'].addTo(map);
   //map.addLayer(drawnItems);

c001ff69d107367e7ba21b50bc8465ce542b160f
Taras Kalapun <t.kalapun@gmail.com>
2017-08-29 11:51:43 +0200

    Update map.js

--- map.js
+++ map.js
@@ -15,7 +15,6 @@ function onLocationError(e) {
 function addLayers() {
   var osm = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
   var mapbox = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/xslim.hgm2p8g2/{z}/{x}/{y}.png');
-  var gmap = new L.Google('ROADMAP');
   var openseamap = new L.TileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png');
   var transas = new L.tileLayer('http://wms.transas.com/tms/1.0.0/tx97/{z}/{x}/{y}.png?token={token}', {
             maxZoom: 17,
@@ -26,7 +25,7 @@ function addLayers() {
         });
   map.addLayer(gmap);
   map.addControl(new L.Control.Layers( 
-    {'OSM':osm, 'Mapbox':mapbox, 'Google':gmap, 'Transas':transas}, 
+    {'OSM':osm, 'Mapbox':mapbox, 'Transas':transas}, 
     {'OpenSeaMap':openseamap}));
 }
 
